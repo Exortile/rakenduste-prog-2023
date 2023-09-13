33f4f03655eaa35bfac018ef9bb41bc2f0d55eee
Exortile <xortile@gmail.com>
2023-09-13 12:48:57 +0300

    13.09 tund, lisatud moned lihtsad funktsioonid

---
+++ lessons/13.09.2023/basics.js
@@ -0,0 +1,10 @@
+function findAnIndex(array, element) {
+    return array.indexOf(element);
+}
+
+function addNumbers(n1, n2) {
+    return n1 + n2;
+}
+
+const numbrid = [0, 3, 13, 12, 8, 7];
+console.log(findAnIndex(numbrid, 13));
