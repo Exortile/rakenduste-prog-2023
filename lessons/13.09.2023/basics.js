64b7b6d499d1baac584cd1de64a88b9767ba48ae
Exortile <xortile@gmail.com>
2023-09-13 15:12:41 +0300

    veel basic asju lisatud

--- lessons/13.09.2023/basics.js
+++ lessons/13.09.2023/basics.js
@@ -45,13 +45,54 @@ const numberArray = [1, 3, 5, 2, 7]
 // .map(() => {})
 
 // liita igale elemendile 5
-const newAddedArray = numberArray.map((elem) => elem + 5)
+const newAddedArray = numberArray.map((elem, index, array) => {
+    // {} on object (nagu hashmap), key value paarid
+    console.log({
+        elem, // andes variable'i, moodustab automaatselt "elem": elem (tema v22rtus)
+        newElement: elem + 5, // tavaline key: value, newElement l2heb automaatselt stringiks
+        "index": index, // k6ige tavalisem key: value
+        array: array // saab ka nii pikemalt
+    })
+
+    return elem + 5
+})
 
 // filter kutsub v2lja callbacki iga array elemendi jaoks
 // callback tagastab booli, mis m22rab 2ra kas lisada see element uude arraysse v6i mitte
 
 
-const filteredArray = numberArray.filter((elem) => elem == 3)
+// uus array kus on ainult elemendid mis on v2iksemad kui 4
+const filteredArray = numberArray.filter((elem, index, array) => elem < 4)
+
+
+const names = ["Juku", "Mari", "Jaan", "Anni"]
+
+const reverseString = str => str.split("").reverse().join("")
+
+// kui tahad tagastada arrow funktsiooniga objecti, pane object sulgudesse
+// muidu arvab et sa tahad funktsiooni keha kirjutada
+const data = names.map(name => ({
+    name,
+    age: name.length + 20,
+    email: `${name.toLowerCase()}@company.com`,
+    address: `${name} Street 55`,
+    username: reverseString(name)
+}))
+
+// tahan lisada uue key value pair
+
+// "..." on nii nimetatud spread syntax
+// ...data[0] v6tab k6ik selle objecti key value ja copyb ymber
+const newJuku = {
+    ...data[0],
+    height: 175
+}
+
+// kui soov yle kirjutada vanas objektis
+const evenNewerJuku = {
+    ...data[0],
+    age: 99
+}
 
 
 /*
@@ -63,4 +104,4 @@ console.log(findAnIndex(numbrid, 13))
 //console.log(addArrowShort(3, 5))
 console.log(addNested3(3)(5))
 console.log(nameString("Sten"))
-console.log(filteredArray)
+console.log({ filteredArray, evenNewerJuku })
